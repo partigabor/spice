89df2080ad4f4602173035839f061af9e27af766
Gabor Parti <partigaborhk@gmail.com>
2022-11-16 13:42:58 +0800

    separate database

--- code/hello.py
+++
@@ -1,7 +0,0 @@
-def fn1():
-    p = "Hello world 1!"
-    print(p)
-
-def fn2():
-    p = "Hello world 2!"
-    print(p)
--- code/move_spice_mds.bat
+++
@@ -1,1 +0,0 @@
-move "C:\Users\parti\GitHub\spice\code\temp\*.md" "C:\Users\parti\GitHub\spice\content\docs\spices"
